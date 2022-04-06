f5a136daa3808c4ee802dbbc7e73c41ba9a3ce36
BrunoRepelex <brunorepelex@gmail.com>
2022-04-06 01:06:32 -0300

    making route post to cadastro

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router;
+const fs = require('fs');
+const { v4: uuid} = require('uuid');
 
 
 const cadastroController ={
@@ -7,8 +9,53 @@ const cadastroController ={
     response.render('cadastro-pessoas')},
  
     pet : (request, response)=>{
-    response.render('cadastro-pet')}
+    response.render('cadastro-pet')},
+
+    novoUsuarios: (request,response)=>{
+        const novoArquivoUsuario = 'usuario.json';
+
+        const usuarioArquivo = fs.readFileSync(novoArquivoUsuario);
+        const usuarioJSON = JSON.parse(usuarioArquivo);
+
+        const novoUsuario = {
+            id: uuid(),
+            ...request.body
+        }
+
+        usuarioJSON.push(novoUsuario);
+
+        console.log(request.body);
+        console.log(novoUsuario);
+
+        fs.writeFileSync(novoArquivoUsuario, JSON.stringify(usuarioJSON));
+        
+        response.render('perfil',{
+            title: 'Express',
+        });
+    },
+    novoAnimais:(request,response)=>{
+        const novoArquivoAnimal = 'animais.json';
+
+        const animalArquivo = fs.readFileSync(novoArquivoAnimal);
+        const animalJSON = JSON.parse(animalArquivo);
+
+        const novoAnimal = {
+            id: uuid(),
+            ...request.body
+        }
+
+        animalJSON.push(novoAnimal);
+
+        console.log(request.body);
+        console.log(novoAnimal);
+
+        fs.writeFileSync(novoArquivoAnimal, JSON.stringify(animalJSON));
+        
+        response.render('perfil',{
+            title: 'Express',
+        });
+    }
 };
 
 
-module.exports = cadastroController;
+module.exports = cadastroController
