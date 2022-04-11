36a1cf967de47a5526489a2522be06e5f4f3d35c
BrunoRepelex <brunorepelex@gmail.com>
2022-04-11 11:47:01 -0300

    login

---
+++ src/routes/loginRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const loginController = require('../controllers/loginController');
+const router = express.Router();
+
+
+router.get('/',loginController.login);
+
+router.post('/',loginController.logar);
+
+
+module.exports = router;
