9b8ff6b116255812879c4b768937b337dd42c05d
Mithras de Luca <mdlucata@gmail.com>
2022-06-22 14:43:42 -0300

    validação middleware

--- src/controllers/doandoController.js
+++ src/controllers/doandoController.js
@@ -1,5 +1,5 @@
-const { Animal } = require('../models');
-const { Foto } = require('../models');
+const { Animal, Foto } = require('../models');
+const { check, validationResult, body } = require('express-validator')
 
 const doandoController = {
     pet: (_, response) => {
@@ -7,38 +7,47 @@ const doandoController = {
     },
 
     novoAnimais: async (request, response) => {
-        const { nome, tipoDePet, raca, tamanhoDoPet, genero, dataNascimento, corPredominante, pelagem, vacinado, castrado } = request.body;
+        
+        console.log(validationResult(request))
+        const listaErros = validationResult(request);
 
-        const { id } = request.session.usuarioEncontrado;
+        if (listaErros.isEmpty()) {
 
-        const imagens = request.files
+            const { nome, tipoDePet, raca, tamanhoDoPet, genero, dataNascimento, corPredominante, pelagem, vacinado, castrado } = request.body;
 
-        const animal = await Animal.create({
-            raca,
-            nome,
-            vacinado,
-            castrado,
-            porte: tamanhoDoPet,
-            cor: corPredominante,
-            tipo: tipoDePet,
-            genero,
-            idade: dataNascimento,
-            pelagem,
-            usuario_id: id,
-        });
+            const { id } = request.session.usuarioEncontrado;
 
-        await Promise.all(imagens.map(element =>
-            Foto.create({
-                nome: element.filename,
-                caminho: '../uploads/',
-                criado_em: `${new Date()}`,
-                animal_id: animal.id
-            })
-        ));
+            const animal = await Animal.create({
+                raca,
+                nome,
+                vacinado,
+                castrado,
+                porte: tamanhoDoPet,
+                cor: corPredominante,
+                tipo: tipoDePet,
+                genero,
+                idade: dataNascimento,
+                pelagem,
+                usuario_id: id,
+            });
 
-        request.session.autorizado = true;
-        response.redirect('/perfil');
-    }
+            const imagens = request.files
+
+            await Promise.all(imagens.map(element =>
+                Foto.create({
+                    nome: element.filename,
+                    caminho: '../uploads/',
+                    criado_em: `${new Date()}`,
+                    animal_id: animal.id
+                })
+            ));
+            request.session.autorizado = true;
+             response.redirect('/perfil');
+        
+        } else {
+             response.redirect('/doando/pet')
+        }
+    },
 };
 
 module.exports = doandoController
--- src/middlewares/cadastroPetValidator.js
+++ src/middlewares/cadastroPetValidator.js
@@ -1,10 +1,20 @@
 const { check, validationResult, body } = require('express-validator');
+const { options } = require('../routes/doandoRouter');
 
 const cadastroPetValidator = [
-    body('tipo').notEmpty().withMessage('Deve preencher o nome').bail(),
-    body('raca').notEmpty().withMessage('Deve preencher o nome').bail(),
-    body('tamanho').notEmpty().bail(),
-    body('genero').notEmpty().bail()
-]
+    body('nome').notEmpty().withMessage('Deve preencher o nome').bail(),
+    body('tipoDePet').isString().withMessage('Deve preencher o tipo').bail(),
+    body('raca').notEmpty().withMessage('Deve preencher a raça').bail(),
+    body('tamanhoDoPet').isString().withMessage('Deve preencher o tamanho').bail(),
+    body('genero').isString().withMessage('Deve preencher o genero').bail(),
+    body('dataNascimento').notEmpty().withMessage('Deve preencher a data de nascimento').bail(),
+    body('corPredominante').isString().withMessage('Deve preencher a cor predominante').bail(),
+    body('pelagem').isString().withMessage('Deve preencher a pelagem').bail(),
+    body('vacinado').isString().withMessage('Deve selecionar o campo correspondente').bail(),
+    body('castrado').isString().withMessage('Deve selecionar o campo correspondente').bail(),
+    body('fotos').custom(options).withMessage('Deve selecionar no mínimo 2 imagens ou no máximo 4 imagens').bail(),
+];
+
+
 
 module.exports = cadastroPetValidator;
